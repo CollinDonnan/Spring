82a9621bf1a515c253fed9873cb81fd01aaf094a
cmidon <collinmdonnan@gmail.com>
2025-01-14 16:29:24 -0500

    Adding Validations

--- src/main/java/com/example/Spring/product/Exceptions/ProductNotFoundExecption.java
+++ src/main/java/com/example/Spring/product/Exceptions/ProductNotFoundExecption.java
@@ -1,4 +1,11 @@
 package com.example.Spring.product.Exceptions;
 
-public class ProductNotFoundExecption {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ProductNotFoundExecption extends RuntimeException{
+    public ProductNotFoundExecption() {
+        super("Product Not Found");
+    }
 }
---
+++ src/main/java/com/example/Spring/product/Validators/ProductValidator.java
@@ -0,0 +1,4 @@
+package com.example.Spring.product.Validators;
+
+public class ProductValidator {
+}
